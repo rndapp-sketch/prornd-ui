e989469f920266db8353beb3566de3bb9b408dbe
okramjimmy <flydonkeygod@gmail.com>
2026-05-21 12:22:43 +0530

    mythos_omni_v0.3: update ecosystem config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,10 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: "prornd-ui-mythos_omni_v0.2",
+      name: "prornd-ui-mythos_omni_v0.3",
       script: "npm",
       args: "run dev",
-      cwd: "/home/prornd/Projects/mythos_omni_v0.2/prornd-ui",
+      cwd: "/home/prornd/Projects/mythos_omni_v0.3/prornd-ui",
       interpreter: "none",
       env: {
         NODE_ENV: "development",
